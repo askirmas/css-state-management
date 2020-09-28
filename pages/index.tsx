ddd8822e0cf53c3307c3584e19e97385f7c95875
Andrii Kirmas <kirmas@phystech.edu>
2020-09-28 18:00:27 +0300

    Update Menu example with additional info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,39 @@
-const MenuViewer = <div className="Menu Viewer">
-  <div>1</div>
-  <div>2</div>
-  <div>3</div>
-</div>
+import type { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react"
+
+type tDivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+
 export default Start
 
 function Start() {
   return <main>
     <h2>Menu</h2>
     <section>
+      <h3>Boolean</h3>
       <article>
         <input type="checkbox" id="menu" className="Menu Model Boolean"/>
         <label htmlFor="menu" className="Menu Controller"/>
-        {MenuViewer}
+        <MenuViewer/>
       </article>
+      <h3>FocusWithin</h3>
       <article>
         <div className="Menu Model FocusWithin">
           <div className="Menu Controller" tabIndex={0}/>
-          {MenuViewer}
+          <MenuViewer/>
         </div>
       </article>
+      <h3>Hover</h3>
       <article>
         <div className="Menu Model Hover">
           <div className="Menu Controller"/>
-          {MenuViewer}
+          <MenuViewer/>
         </div>
       </article>
     </section>
+    <h3>TODO</h3>
+    <ul>
+      <li>Nested</li>
+      <li>Hide </li>
+    </ul>
     <h2>Collapser/Expander</h2>
     <section>
       <div>:checked</div>
@@ -48,4 +55,22 @@ function Start() {
       <div></div>
     </section>
   </main>
+}
+
+function MenuViewer ({className, ...props}: tDivProps) {
+  const block = "Menu"
+  , length = 3
+  , children: ReactNode[] = new Array(length)
+  
+  for (let key = length; key--;)
+    children[key] = <div {...{
+      key,
+      "className": `${block} Item ${className ?? ""}`,
+      ...props
+    }}/>
+
+
+  return <div className={`${block} Viewer`}>{
+    children
+  }</div>
 }
